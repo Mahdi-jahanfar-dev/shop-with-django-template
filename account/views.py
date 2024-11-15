4fa5d2c6fc50285243b83cc7eb484c870853080b
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-11-16 01:48:47 +0330

    user register view get method created

--- account/views.py
+++ account/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-
+from . import forms
 
 class UserRegisterView(View):
 
+    form_class = forms.RegisterForm
+
     def get(self, request):
-        pass
+        form = self.form_class()
+        return render(request, 'account/register.html', {'form': form})
     def post(self, request):
         pass
-    
