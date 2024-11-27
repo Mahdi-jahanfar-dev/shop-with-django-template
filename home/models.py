cbf5ab7ee44cbf49c5f15609fd365bb08cf5db6d
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-11-27 22:58:22 +0330

    product and category models added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(null=True, blank=True)
+
+
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.name))
+        super(Category, self).save(*args, **kwargs)
+        return self
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.ManyToManyField(Category)
+    image = models.ImageField(upload_to='media/')
+    description = models.TextField()
+    slug = models.SlugField(null=True, blank=True)
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Product'
+
+    def __str__(self):
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.name))
+        super(Product, self).save(*args, **kwargs)
+        return self
+
+
+
